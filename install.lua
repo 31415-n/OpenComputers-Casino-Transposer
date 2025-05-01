49912a8a85ccb2f21e5e28f0cfecdbf6959cebb8
31415-n <lemuran2@gmail.com>
2025-05-01 19:39:11 +0700

    Update install.lua

--- install.lua
+++ install.lua
@@ -1,4 +1,4 @@
-local REPOSITOTY = "https://raw.githubusercontent.com/krovyaka/OpenComputers-Casino/master"
+local REPOSITOTY = "https://github.com/31415-n/OpenComputers-Casino-Transposer/edit/master"
 
 local shell = require("shell")
 shell.execute("wget -fq " .. REPOSITOTY .. "/launcher.lua /home/1.lua")
