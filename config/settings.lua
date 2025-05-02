b1fd859fd1438bdca7fef6a8cde7c57962d50783
Ваше Имя <you@example.com>
2025-05-02 15:47:07 +0700

    update settings

--- config/settings.lua
+++ config/settings.lua
@@ -1,15 +1,15 @@
 local settings = {}
 
-settings.REPOSITORY = "https://raw.githubusercontent.com/krovyaka/OpenComputers-Casino/master"
+local REPOSITOTY = "https://github.com/31415-n/OpenComputers-Casino-Transposer/edit/master"
 settings.TITLE = "Приветствуем ваc у нас в казино"
-settings.ADMINS = { "krovyak", "Durex77" }
+settings.ADMINS = {}
 
 -- CHEST - Взаимодействие сундука и МЕ сети
 -- TRANSPOSER - Взаимодействие транспозера и МЕ сети # FOR 1.12.2 ONLY!
 -- PIM - Взаимодействие PIM и МЕ сети
 -- CRYSTAL - Взаимодействие кристального сундука и алмазного сундука
 -- DEV - Оплата не взимается, награда не выдается, не требует внешних компонентов
-settings.PAYMENT_METHOD = "PIM"
+settings.PAYMENT_METHOD = "TRANSPOSER"
 settings.CONTAINER_PAY = "DOWN"
 settings.CONTAINER_GAIN = "UP"
 
